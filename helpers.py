ca2edcb8d8bfe6f409ebb4c960d7b86b3ff3a6c8
Ali Turfah <aturfah@gmail.com>
2022-06-04 18:14:13 -0400

    Get the hex to match in-game files

--- helpers.py
+++ helpers.py
@@ -15,13 +15,12 @@ def load_skillname_hex_maps():
         name, _, hex_id = line.split("\t")
         name = name.strip()
         hex_id = hex_id.strip()
+
+        ## 0DB => DB00 so it actually works
+        hex_id = "0" + hex_id[1:] + hex_id[0]
+
         if name in player_skills or name in enemy_grimoire_skills:
             name_to_hex[name] = hex_id
             hex_to_name[hex_id] = name
 
     return name_to_hex, hex_to_name
-
-if __name__ == "__main__":
-    n_to_h, h_to_n = load_skillname_hex_maps()
-    pprint(n_to_h)
-    pprint(h_to_n)
