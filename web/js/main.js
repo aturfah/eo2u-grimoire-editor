d3a7237f3cdf4041c56c38e14c9cfe2e2acc550d
Ali Turfah <aturfah@gmail.com>
2022-06-12 13:56:51 -0400

    Clean up code a bit

--- web/js/main.js
+++ web/js/main.js
@@ -1,27 +1,27 @@
-function loadMethod() {
-  eel.load_file()
-  setGrimoireDropdown()
-}
-
-document.getElementById("load-button").addEventListener("click", ()=>{loadMethod()}, false);
-document.getElementById("save-button").addEventListener("click", ()=>{eel.get_date()}, false);
-
-
 eel.expose(prompt_alerts);
 function prompt_alerts(description) {
   alert(description);
 }
 
+// Set the elements in the dropdown list
 eel.expose(setGrimoireDropdown)
 function setGrimoireDropdown(grimoireList) {
-  // console.log(grimoireList)
-  const grimoireDropdown = document.getElementById("grimoire-dropdown")
+  const grimoireDropdown = document.getElementById("grimoire-dropdown");
 
   grimoireList.forEach((name, idx) => {
-    console.log(idx, name)
     let opt = document.createElement("option");
     opt.value = idx;
     opt.innerHTML = name;
     grimoireDropdown.appendChild(opt);
   })
 }
+
+// Load the file from disk
+function loadMethod() {
+  eel.load_file();
+  setGrimoireDropdown();
+}
+
+// Assign functionality to buttons
+document.getElementById("load-button").addEventListener("click", ()=>{loadMethod()}, false);
+document.getElementById("save-button").addEventListener("click", ()=>{eel.get_date()}, false);
