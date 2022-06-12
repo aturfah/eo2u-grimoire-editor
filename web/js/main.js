88abd2e94c3c689b739a99e2b0a3eeb95ec0dbf8
Ali Turfah <aturfah@gmail.com>
2022-06-12 12:22:54 -0400

    Assign events to elements

--- web/js/main.js
+++ web/js/main.js
@@ -1,7 +1,6 @@
-document.getElementById("button-name").addEventListener("click", ()=>{eel.get_random_name()}, false);
-document.getElementById("button-number").addEventListener("click", ()=>{eel.get_random_number()}, false);
-document.getElementById("button-date").addEventListener("click", ()=>{eel.get_date()}, false);
-document.getElementById("button-ip").addEventListener("click", ()=>{eel.get_ip()}, false);
+document.getElementById("load-button").addEventListener("click", ()=>{eel.get_random_number()}, false);
+document.getElementById("save-button").addEventListener("click", ()=>{eel.get_date()}, false);
+
 
 eel.expose(prompt_alerts);
 function prompt_alerts(description) {
